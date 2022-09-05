271465dbc8c98e5e15b64a8e8dbe1c28fae6cdbf
yego <yegokipkemoi254@gmail.com>
2022-09-05 23:53:20 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const main = document.querySelector("#main-content")
+  console.log(main);
+
+  const taskForm = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
+   taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    const newTask = document.querySelector("#new-task-description").value
+    
+  taskList.innerHTML += `
+  <li>${newTask}</li>
+  <button data-action = "delete"> Remove </button>
+  `
+
+  // const taskItem = document.createElement("li")
+  // taskItem.innerText = newTask
+
+  // taskList.appendChild(taskItem)
+  taskForm.reset()
+   })
+taskList.addEventListener("click", function(e) {
+  console.log(e.target)
+  if(e.target.dataset.action === "delete"){
+    e.target.parentElement.remove()
+  }
+})
+
+// });
